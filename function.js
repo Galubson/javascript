c87d963a9fd5a56fd286046d43d8342263fda07a
Galubson <43521939+Galubson@users.noreply.github.com>
2018-09-23 20:51:13 +0200

    Add files via upload
    functions / numbers divisible by three

---
+++ function.js
@@ -0,0 +1,15 @@
+/*funkcje / liczby podzielne przez trzy
+functions / numbers divisible by three
+*/
+function three(z)
+{
+	for(var i=0;i<=z;i++)
+	{
+		if((i%3)!=0)
+		continue;
+		console.log(i);
+	}
+	console.log("\n");
+}
+three(90);
+three(120);
