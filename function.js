927b87c6bd9eda03e0a2792b10a138ecf9a06c88
Galubson <43521939+Galubson@users.noreply.github.com>
2018-09-23 20:52:36 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,10 +1,8 @@
 /*funkcje / liczby podzielne przez trzy
 functions / numbers divisible by three
 */
-function three(z)
-{
-	for(var i=0;i<=z;i++)
-	{
+function three(z){
+	for(var i=0;i<=z;i++){
 		if((i%3)!=0)
 		continue;
 		console.log(i);
